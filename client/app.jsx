eb65ea27cf3f46e2f90c4c481dc27cd50b2d6472
Joseph Lee <teddylou0215@gmail.com>
2022-07-18 22:12:24 +0000

    modify app.jsx to include Hero component

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Home from './pages/home';
+import DataSciencePage from './pages/data-science';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return <DataSciencePage />;
   }
 }
