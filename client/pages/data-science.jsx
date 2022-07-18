579ce00f3d5d4d1d1757b7924297cf5671917278
Joseph Lee <teddylou0215@gmail.com>
2022-07-18 22:29:45 +0000

    import Hero component to DataSciencePage component

---
+++ client/pages/data-science.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hero from '../components/hero';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function DataSciencePage(props) {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
