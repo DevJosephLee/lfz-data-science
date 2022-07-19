e2bcfed3e809fc2aea19453dd04c994bf08f9967
Joseph Lee <teddylou0215@gmail.com>
2022-07-19 23:40:44 +0000

    change file name

--- client/pages/data-science.jsx
+++ client/pages/data-science.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Hero from '../components/hero';
-import Essentials from '../components/essential';
+import Essentials from '../components/essentials';
 
 export default function DataSciencePage(props) {
   return (
