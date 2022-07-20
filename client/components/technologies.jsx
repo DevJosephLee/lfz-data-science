a5c610233e44414464eb0b97a6f9d3132678c154
Joseph Lee <teddylou0215@gmail.com>
2022-07-20 00:04:35 +0000

    add required images

---
+++ client/components/technologies.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Technologies() {
+  return (
+    <div className="container">
+      <div className="text-center">
+        <h1 className="header">Technologies & Concepts</h1>
+      </div>
+      <div className="d-flex">
+      </div>
+    </div>
+  );
+}
